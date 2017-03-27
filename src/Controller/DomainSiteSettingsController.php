629c340aaf243c26058a15f09bce19f1433c6612
ALOK SINGH NARWARIA <aloknarwaria@gmail.com>
2017-03-27 18:01:08 +0530

    Working on form submition

--- src/Controller/DomainSiteSettingsController.php
+++ src/Controller/DomainSiteSettingsController.php
@@ -13,32 +13,30 @@ use Drupal\Core\Url;
  */
 class DomainSiteSettingsController extends ControllerBase {
 
-    /**
-     * 
-     * @return string
-     */
-    public function domains_list() {
-        $domains = \Drupal::service('domain.loader')->loadMultipleSorted();
-        $rows = [];
-        /** @var \Drupal\domain\DomainInterface $domain */
-        foreach ($domains as $domain) {
-            
-            $row = [
-                $domain->label(),
-                $domain->id(),
-                Link::fromTextAndUrl(t('Edit'), Url::fromUri('internal://admin/config/domain_site_settings/'. $domain->id() .'/config')),
-            ];
-            $rows[] = $row;
-            // print_r($row);
-        }
-        // Build a render array which will be themed as a table.
-        $build['pager_example'] = array(
-            '#rows' => $rows,
-            '#header' => array(t('Name'), t('Hostname'), t('Edit Settings')),
-            '#type' => 'table',
-            '#empty' => t('No domain record found.'),
-        );
-        return $build;
+  /**
+   * 
+   * @return string
+   */
+  public function domains_list() {
+    $domains = \Drupal::service('domain.loader')->loadMultipleSorted();
+    $rows = [];
+    /** @var \Drupal\domain\DomainInterface $domain */
+    foreach ($domains as $domain) {
+      $row = [
+        $domain->label(),
+        $domain->id(),
+        Link::fromTextAndUrl(t('Edit'), Url::fromRoute('domain_site_settings.config_form', array('domain_id' => $domain->id()))),
+      ];
+      $rows[] = $row;
     }
+    // Build a render array which will be themed as a table.
+    $build['pager_example'] = array(
+      '#rows' => $rows,
+      '#header' => array(t('Name'), t('Hostname'), t('Edit Settings')),
+      '#type' => 'table',
+      '#empty' => t('No domain record found.'),
+    );
+    return $build;
+  }
 
 }
--- src/Form/DomainConfigSettingsForm.php
+++ src/Form/DomainConfigSettingsForm.php
@@ -7,21 +7,66 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+use Drupal\Core\Path\AliasManagerInterface;
+use Drupal\Core\Path\PathValidatorInterface;
+use Drupal\Core\Routing\RequestContext;
+
 /**
  * Class DomainConfigSettingsForm.
  *
  * @package Drupal\domain_site_settings\Form
  */
 class DomainConfigSettingsForm extends ConfigFormBase {
-  public function __construct(
-    ConfigFactoryInterface $config_factory
-    ) {
+  /**
+   * The path alias manager.
+   *
+   * @var \Drupal\Core\Path\AliasManagerInterface
+   */
+  protected $aliasManager;
+
+  /**
+   * The path validator.
+   *
+   * @var \Drupal\Core\Path\PathValidatorInterface
+   */
+  protected $pathValidator;
+
+  /**
+   * The request context.
+   *
+   * @var \Drupal\Core\Routing\RequestContext
+   */
+  protected $requestContext;
+
+  /**
+   * Constructs a SiteInformationForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Path\AliasManagerInterface $alias_manager
+   *   The path alias manager.
+   * @param \Drupal\Core\Path\PathValidatorInterface $path_validator
+   *   The path validator.
+   * @param \Drupal\Core\Routing\RequestContext $request_context
+   *   The request context.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, AliasManagerInterface $alias_manager, PathValidatorInterface $path_validator, RequestContext $request_context) {
     parent::__construct($config_factory);
+
+    $this->aliasManager = $alias_manager;
+    $this->pathValidator = $path_validator;
+    $this->requestContext = $request_context;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('config.factory')
+      $container->get('config.factory'),
+      $container->get('path.alias_manager'),
+      $container->get('path.validator'),
+      $container->get('router.request_context')
     );
   }
 
@@ -46,10 +91,75 @@ class DomainConfigSettingsForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('domain_site_settings.domainconfigsettings');
+    $domain_id = $this->getRequest()->get('domain_id');
+
+    $site_config = $this->config('system.site');
+    $site_mail = $site_config->get('mail');
+    if (empty($site_mail)) {
+      $site_mail = ini_get('sendmail_from');
+    }
+
+    $form['site_information'] = array(
+      '#type' => 'details',
+      '#title' => t('Site details'),
+      '#open' => TRUE,
+    );
+    $form['site_information']['site_name'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Site name'),
+      '#default_value' => $site_config->get('name'),
+      '#required' => TRUE,
+    );
+    $form['site_information']['site_slogan'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Slogan'),
+      '#default_value' => $site_config->get('slogan'),
+      '#description' => t("How this is used depends on your site's theme."),
+    );
+    $form['site_information']['site_mail'] = array(
+      '#type' => 'email',
+      '#title' => t('Email address'),
+      '#default_value' => $site_mail,
+      '#description' => t("The <em>From</em> address in automated emails sent during registration and new password requests, and other notifications. (Use an address ending in your site's domain to help prevent this email being flagged as spam.)"),
+      '#required' => TRUE,
+    );
+    $form['front_page'] = array(
+      '#type' => 'details',
+      '#title' => t('Front page'),
+      '#open' => TRUE,
+    );
+    $front_page = $site_config->get('page.front') != '/user/login' ? $this->aliasManager->getAliasByPath($site_config->get('page.front')) : '';
+    $form['front_page']['site_frontpage'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Default front page'),
+      '#default_value' => $front_page,
+      '#size' => 40,
+      '#description' => t('Optionally, specify a relative URL to display as the front page. Leave blank to display the default front page.'),
+      '#field_prefix' => $this->requestContext->getCompleteBaseUrl(),
+    );
+    $form['error_page'] = array(
+      '#type' => 'details',
+      '#title' => t('Error pages'),
+      '#open' => TRUE,
+    );
+    $form['error_page']['site_403'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Default 403 (access denied) page'),
+      '#default_value' => $site_config->get('page.403'),
+      '#size' => 40,
+      '#description' => t('This page is displayed when the requested document is denied to the current user. Leave blank to display a generic "access denied" page.'),
+    );
+    $form['error_page']['site_404'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Default 404 (not found) page'),
+      '#default_value' => $site_config->get('page.404'),
+      '#size' => 40,
+      '#description' => t('This page is displayed when no other content matches the requested document. Leave blank to display a generic "page not found" page.'),
+    );
     $form['domain_id'] = [
       '#type' => 'hidden',
       '#title' => $this->t('Domain ID'),
-      '#default_value' => $config->get('domain_id'),
+      '#default_value' => $domain_id,
     ];
     return parent::buildForm($form, $form_state);
   }
@@ -67,9 +177,16 @@ class DomainConfigSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    $this->config('domain_site_settings.domainconfigsettings')
-      ->set('domain_id', $form_state->getValue('domain_id'))
-      ->save();
+//    $this->config('domain_site_settings.domainconfigsettings')
+//      ->set('domain_id', $form_state->getValue('domain_id'))
+//      ->save();
+    $domains = $this->domainLoader->loadMultipleSorted();
+    $config = $this->config('domain_site_settings.domainconfigsettings');
+    foreach ($domains as $domain) {
+      $domainId = $domain->id();
+      $config->set($domainId, $form_state->getValue($domainId));
+    }
+    $config->save();
   }
 
 }
